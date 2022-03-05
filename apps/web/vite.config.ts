266bdcaa33747bce3cfa2455042e7d6502be063c
Chris Legaxy <chrislegaxy@gmail.com>
2022-03-06 00:38:13 +0700

    feat(web): add autoimport for vue & components

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,7 +1,31 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-// https://vitejs.dev/config/
+import path from "path";
+
+import AutoImport from "unplugin-auto-import/vite";
+import Component from "unplugin-vue-components/vite";
+
 export default defineConfig({
-  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@/": `${path.resolve(__dirname, "src")}/`,
+    },
+  },
+  plugins: [
+    AutoImport({
+      resolvers: [],
+      imports: ["vue", "vue-router", "vuex"],
+      dts: "src/auto-imports.d.ts",
+    }),
+
+    Component({
+      resolvers: [],
+      extensions: ["vue"],
+      deep: true,
+      dts: "src/components.d.ts",
+    }),
+
+    vue(),
+  ],
 });
